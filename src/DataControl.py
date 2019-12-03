84b227a9a672c120dcb530fbaf84e272061dd773
Lewis Mojica <mojicalewis@gmail.com>
2019-12-03 12:19:52 -0400

    Ahora se crea reporte de precio al introducir todas las informaciones

--- src/DataControl.py
+++ src/DataControl.py
@@ -1,7 +1,7 @@
 import json
+import datetime
 
 print_job = {}
-pricing = {}
 pam = None
 
 def updatePam():
@@ -17,7 +17,6 @@ def getPrintData():
         except KeyError:
             print("La impresora <", inn, "> no existe, ingrese una impresora válida")
             nextData()
-
     nextData()
             
     print_job["description"] = input("Ingrese descripción de impresión > ")
@@ -29,43 +28,77 @@ def getPrintData():
         except ValueError:
             print("Formato incorrecto, ¿olvidaste poner :?")
             nextData()
-
     nextData()
 
     def nextData():
         try:
-            [hh,mm] = input("*Tiempo de trabjo (humano) (format: hh:mm) > ").split(':')
+            [hh,mm] = input("*Tiempo de trabajo (humano) (format: hh:mm) > ").split(':')
             print_job["human time"] = int(hh) + int(mm)/60
         except ValueError:
             print("Formato incorrecto, ¿olvidaste poner :?")
             nextData()
-        
     nextData()
 
     def nextData():
         try:
-            print_job["material lenght"] = float(input("Ingrese los metros de filamento > "))
+            print_job["filament lenght"] = float(input("Ingrese los metros de filamento > "))
         except ValueError:
             print("Formato incorrecto. Solo números")
             nextData()
-
     nextData()
 
 
     def nextData():
         try:
-            inn = input("Ingrese el material > ").upper()
-            print_job["material"] = pam["materials"][inn][0]
+            inn = input("Ingrese el material > ").lower()
+            if len(pam["materials"][inn]) == 1:
+                print_job["material"] = pam["materials"][inn][0]
+            else:
+                def getMat():
+                    try:
+                        print('Listas de carretes de', inn, ':')
+                        for iter in pam["materials"][inn]:
+                            print('->', iter['name'])
+                        carrete = input('Espefique el carrete > ')
+
+                        print_job["material"] = next(item for item in pam['materials'][inn] if item['name'].lower() == carrete.lower())
+                    except StopIteration:
+                        print("Carrete <", carrete, "> no existente")
+                        getMat()
+                getMat()
+
         except KeyError:
             print("Material inexistente")
             nextData()
-
     nextData()
 
-    print(print_job)
+    calculatePrice()
 
-def calculatePrice(material,printer,job):
-    pass
+def calculatePrice():
+    global print_job
+    print_job["pricing"] = {}
+    try:
+        print_job['pricing']['electricity cost'] = print_job["material"]["printers consumptions"][print_job["printer"]["name"]]*print_job["printing time"]
+    except KeyError:
+        print_job['pricing']['electricity cost'] = print_job["printer"]["default consumption"]*print_job["printing time"]
 
-    
 
+    print_job['pricing']['profit margin'] = 1 
+    print_job['pricing']['human labor'] = print_job['human time']*pam['human time price']
+    print_job['pricing']['filament cost'] = print_job['material']['cost']*print_job['filament lenght']
+    print_job['pricing']['printer operation cost'] = print_job["printer"]["operation cost"]*print_job["printing time"]
+    print_job['pricing']['net cost'] = print_job['pricing']['human labor']\
+        + print_job['pricing']['filament cost']\
+        + print_job['pricing']['printer operation cost']\
+        + print_job['pricing']['printer operation cost']\
+        + print_job['pricing']['electricity cost']
+
+    print_job['pricing']['date'] = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    print_job['pricing']['client cost'] = (print_job["pricing"]['profit margin'] + 1)*print_job['pricing']['net cost']
+
+    print()
+    print()
+    print()
+    for iter in print_job['pricing']:
+        print()
+        print(iter,':', print_job['pricing'][iter])
